b8153013d7171698471415e3281ac87da796478b
traxhi <graciane_coelho@estudante.sesisenai.org.br>
2023-06-17 11:59:32 -0300

    17/06

--- itens&produtos/publicar.php
+++ itens&produtos/publicar.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login']) || empty($_SESSION['login'])) {
+    header("Location: ../cadastro&login/login.php");
+    exit();
+} 
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
